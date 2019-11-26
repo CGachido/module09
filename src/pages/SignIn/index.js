02974ba2c9f76435a02e81c353982c9a575de2d2
Cassius Souza <cassius.sanches@hotmail.com>
2019-11-25 21:00:24 -0300

    Finshing Go Barber Web

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,7 +1,40 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
+
+import { signInRequest } from '~/store/modules/auth/actions';
+
+import logo from '~/assets/logo.svg';
 
 // import { Container } from './styles';
+const schema = Yup.object().shape({
+  email: Yup.string()
+    .email('Invalid email')
+    .required('Email is a required field'),
+  password: Yup.string().required('Password is a required field'),
+});
 
 export default function SignIn() {
-  return <h1>Gek</h1>;
+  const dispatch = useDispatch();
+  const loading = useSelector(state => state.auth.loading);
+
+  function handleSubmit({ email, password }) {
+    dispatch(signInRequest(email, password));
+  }
+
+  return (
+    <>
+      <img src={logo} alt="GoBarber" />
+
+      <Form schema={schema} onSubmit={handleSubmit}>
+        <Input name="email" type="email" placeholder="E-mail" />
+        <Input name="password" type="password" placeholder="Password" />
+
+        <button type="submit">{loading ? 'Loading ...' : 'Access'}</button>
+        <Link to="/register">Create account</Link>
+      </Form>
+    </>
+  );
 }
--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,7 +1,40 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
 
-// import { Container } from './styles';
+import * as Yup from 'yup';
+import logo from '~/assets/logo.svg';
+
+import { signUpRequest } from '~/store/modules/auth/actions';
+
+const schema = Yup.object().shape({
+  name: Yup.string().required('Name is a required field'),
+  email: Yup.string()
+    .email('Invalid email')
+    .required('Email is a required field'),
+  password: Yup.string()
+    .min(6, 'Password must be at least 6 characters')
+    .required('Password is a required field'),
+});
 
 export default function SignUp() {
-  return <h1>Login</h1>;
+  const dispatch = useDispatch();
+  function handleSubmit({ name, email, password }) {
+    dispatch(signUpRequest(name, email, password));
+  }
+  return (
+    <>
+      <img src={logo} alt="GoBarber" />
+
+      <Form schema={schema} onSubmit={handleSubmit}>
+        <Input name="name" type="text" placeholder="Full name" />
+        <Input name="email" type="email" placeholder="E-mail" />
+        <Input name="password" type="password" placeholder="Password" />
+
+        <button type="submit">Access</button>
+        <Link to="/">Login</Link>
+      </Form>
+    </>
+  );
 }
